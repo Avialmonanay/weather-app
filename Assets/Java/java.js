ed4543043ba06d0a1c8240cec97672213e051a08
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-08-23 22:37:43 -0600

    logHistory and WeatherItem functions created. log history pulls all history items from local storage and appends them onto the page

--- Assets/Java/java.js
+++ Assets/Java/java.js
@@ -1,11 +1,15 @@
 var cityNameLS = []
-
+var listEl = $("#history")
 
 $(".search").click(function (event) {
     var element = event.target
     if (element.matches("button")) {
         var userInput = $(this).children("input").val()
         // console.log(userInput)
+
+        if (!userInput){
+            return
+        }
         localStorage.setItem("currentCity", userInput)
 
         const cityNameStorage = JSON.parse(localStorage.getItem("cityNameLS"))
@@ -21,25 +25,76 @@ $(".search").click(function (event) {
 
 
             cityNameStorage.push(userInput)
-    
+
 
             localStorage.setItem("cityNameLS", JSON.stringify(cityNameStorage));
         }
-
+        weatherItem()
+        logHistory()
         return
-        // pageLoad()
-        // logHistory()
+
+
+
+
+
+
+
+
     }
+
 })
 
 
+function weatherItem() {
+    const currentCityLS = localStorage.getItem("currentCity")
 
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=c64d9c95aa9e442bc0444f33c92c8506", {
+    if (currentCityLS) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + currentCityLS + "&appid=c64d9c95aa9e442bc0444f33c92c8506", {
 
-// })
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     });
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            });
+    }
+}
+
+
+
+
+
+function logHistory() {
+    const cityHistory = JSON.parse(localStorage.getItem("cityNameLS"));
+
+
+    if (!cityHistory) {
+        return
+    }
+
+    else{
+        for (var i = 0; i < cityHistory.length; i++) {
+
+            var nameDisplay = cityHistory;
+
+
+            
+            var listItem = document.createElement("li")
+            listItem.setAttribute("class", "historycard")
+
+            var initial = document.createElement("h2")
+            
+            initial.innerText = nameDisplay[i]
+
+
+            listEl.append(listItem)
+            listItem.append(initial)
+
+        }
+
+    }
+}
+
+weatherItem()
+logHistory()
